92bcb96f82d7e151b0313c7dfa354abedf0a3e56
FloRide1 <florian.reimat@orange.fr>
2023-01-08 10:52:10 +0100

    [LEGION] Update legion machine config

--- desktops/machines/legion.nix
+++ desktops/machines/legion.nix
@@ -1,30 +1,44 @@
-{ pkgs, lib, config, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  # Setup keyfile
+  boot.initrd.secrets = { "/crypto_keyfile.bin" = null; };
+
+  # Enable swap on luks
+  boot.initrd.luks.devices."luks-a3c3f68a-9906-443d-93eb-38952151008e".device =
+    "/dev/disk/by-uuid/a3c3f68a-9906-443d-93eb-38952151008e";
+  boot.initrd.luks.devices."luks-16d3a52c-1210-4ebb-a4fb-165d66fd00c7".device =
+    "/dev/disk/by-uuid/16d3a52c-1210-4ebb-a4fb-165d66fd00c7";
+  boot.initrd.luks.devices."luks-16d3a52c-1210-4ebb-a4fb-165d66fd00c7".keyFile =
+    "/crypto_keyfile.bin";
+
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  boot.supportedFilesystems = [ "ntfs" ];
+
   boot = {
-    initrd.availableKernelModules =
-      [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-    initrd.kernelModules = [ ];
-    kernelModules = [ "kvm-intel" ];
-    extraModulePackages = with config.boot.kernelPackages; [ ];
-
-    supportedFilesystems = [ "ntfs" ];
-
     loader = {
       efi = {
-        efiSysMountPoint = "/boot";
+        efiSysMountPoint = "/boot/efi";
         canTouchEfiVariables = true;
       };
 
-      timeout = 0;
+      #timeout = 0;
       grub = {
         enable = true;
-        device = "nodev";
-        default = "saved";
-        efiSupport = true;
         version = 2;
+        device = "nodev";
+        efiSupport = true;
+        enableCryptodisk = true;
+        useOSProber = true;
       };
-      systemd-boot.configurationLimit = 5;
+      #systemd-boot.configurationLimit = 5;
     };
   };
 
@@ -44,24 +58,23 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/a6195274-7b9c-4209-ad81-b2fb6e84303d";
+      device = "/dev/disk/by-uuid/bfb74672-e281-4e1d-b53c-2c86d19511ea";
       fsType = "ext4";
     };
 
-    "/boot" = {
-      device = "/dev/disk/by-uuid/9C02-28F3";
+    "/boot/efi" = {
+      device = "/dev/disk/by-uuid/6A68-E407";
       fsType = "vfat";
     };
 
     "/storage" = {
-      device = "/dev/disk/by-uuid/bd022d60-aa7c-44af-aaee-9257033dd681";
-      fsType = "ext4";
+      device = "/dev/disk/by-uuid/9C882C65882C405E";
+      fsType = "ntfs";
     };
   };
 
-  # Maybe remove it later :/
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/c52bafff-92c8-4024-bb3c-abc25438d5b9"; }];
+    [{ device = "/dev/disk/by-uuid/23a97d93-88eb-4b3c-9986-7d4ee0a204d9"; }];
 
   nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
   environment.systemPackages = with pkgs; [ plasma-browser-integration ];
