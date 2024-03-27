0f8fddd6104636ec485d35e63149d913be54b386
FloRide1 <florian.reimat@orange.fr>
2024-03-27 23:08:57 +0100

    [HEPHAISTOS] Update nixos-hardware config

--- desktops/machines/hephaistos/framework/13-inch/7040-amd/default.nix
+++ desktops/machines/hephaistos/framework/13-inch/7040-amd/default.nix
@@ -1,13 +1,9 @@
 { config, lib, pkgs, ... }:
 
-let
-  cfg = config.hardware.framework.amd-7040;
-in
-{
-  imports = [
-    ../common
-    ../common/amd.nix
-  ];
+let cfg = config.hardware.framework.amd-7040;
+in {
+  imports =
+    [ ../common ../common/amd.nix ../../../../common/cpu/amd/raphael/igpu.nix ];
 
   options = {
     hardware.framework.amd-7040.preventWakeOnAC = lib.mkOption {
@@ -24,11 +20,6 @@ in
   };
 
   config = {
-    # Newer kernel is better for amdgpu driver updates
-    # Requires at least 5.16 for working wi-fi and bluetooth (RZ616, kmod mt7922):
-    # https://wireless.wiki.kernel.org/en/users/drivers/mediatek
-    boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.1") (lib.mkDefault pkgs.linuxPackages_latest);
-
     services.udev.extraRules = lib.optionalString cfg.preventWakeOnAC ''
       # Prevent wake when plugging in AC during suspend. Trade-off: keyboard wake disabled. See:
       # https://community.frame.work/t/tracking-framework-amd-ryzen-7040-series-lid-wakeup-behavior-feedback/39128/45
